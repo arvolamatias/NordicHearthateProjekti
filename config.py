e8eb0446ebdc99ab8b75381b138e881fc823248c
arvolamatias <125243479+arvolamatias@users.noreply.github.com>
2024-01-19 13:12:18 +0200

    Delete config.py

--- config.py
+++
@@ -1,7 +0,0 @@
-
-
-DIRECTUS_USERNAME = 'EMAIL'
-DIRECTUS_PASSWORD = 'PASSWORD'
-LOGIN_URL ='https://nag39ni9.directus.app/auth/login'
-REFRESH_URL ='https://nag39ni9.directus.app/auth/refresh'
-JSON_URL = 'https://nag39ni9.directus.app/items/feedback'
